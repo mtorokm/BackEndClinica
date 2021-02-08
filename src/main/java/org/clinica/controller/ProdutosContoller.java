9e0fec78f1d88c678df28cf8e4593d5b122c85e0
unknown <matheus.torok98@gmail.com>
2021-02-08 20:40:02 -0300

    GetById e GetByNomeProduto

--- src/main/java/org/clinica/controller/ProdutosContoller.java
+++ src/main/java/org/clinica/controller/ProdutosContoller.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.clinica.model.ProdutosModel;
 import org.clinica.repository.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,12 +22,24 @@ public class ProdutosContoller {
 	private ProdutosRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<ProdutosModel>> getAll(){
+	public ResponseEntity<List<ProdutosModel>> getAll (){
+		
 		return ResponseEntity.ok(repository.findAll());
+
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProdutosModel> GetById(@PathVariable long id) {
+		
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
 	
-	
-	
+	@GetMapping("/nomeProduto/{nomeProduto}")
+	public ResponseEntity<List<ProdutosModel>> GetByNomeProduto(@PathVariable String nomeProduto){
+		return ResponseEntity.ok(repository.findAllByNomeProdutoContainingIgnoreCase(nomeProduto));
+	}
+
 	
 }
